7fb28c0ec11d53c786e800d2c6a22206fce84f58
CodeWontWork <jedansantos727@gmail.com>
2026-05-26 02:30:14 +0800

    yafasfaf

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
-      <Script src="/holo-cards.js" strategy="lazyOnload" />;
+      <Script src="/holo-cards.js" strategy="lazyOnload" />
     </html>
   );
 }
